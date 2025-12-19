1832ef8b1132a23f2667f3e506cdb01e42733316
Tiago Albuquerque <tiagoalbuquerque090@gmail.com>
2025-12-19 15:46:39 -0300

    Ajustes no código do editor.py e editor_config.py

--- editor.py
+++ editor.py
@@ -4,21 +4,24 @@ import pygame
 
 from tkinter import Tk, filedialog
 
+from scripts.editor_config import *
 from scripts.utils import carregar_imagens
 from scripts.tilemap import Tilemap
-
-ESCALA_RENDER = 2.0
 
 
 class Editor:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption('Editor de Levels')
-        self.tela = pygame.display.set_mode((1280, 720))
-        self.display = pygame.Surface((640, 360))
+        pygame.display.set_caption(LEGENDA)
+        self.tela = pygame.display.set_mode(EDITOR_RES)
+        self.display = pygame.Surface((DISPLAY_L, DISPLAY_A))
         self.relogio = pygame.time.Clock()
 
-        self.mpos = None; self.camera = None; self.pos_tile = None
+        self.rodando = True
+
+        self.m_pos = None
+        self.camera = None
+        self.pos_tile = None
 
         self.assets = {
             'decor': carregar_imagens('tiles/decor'),
@@ -30,17 +33,14 @@ class Editor:
 
         self.movimento = [False, False, False, False]
 
-        self.mapa_jogo = Tilemap(self, tamanho_tile=16)
-        self.tamanho_tile = self.mapa_jogo.tamanho_tile
+        self.mapa_jogo = Tilemap(self, TILE_SIZE)
 
         try:
-            self.mapa_jogo.carregar('mapa.json')
+            self.mapa_jogo.carregar(NOME_SAVE)
         except FileNotFoundError:
             pass
 
         self.scroll = [0, 0]
-
-        self.selecao = [None, None]
 
         self.lista_tiles = list(self.assets)
         self.grupo_tile = 0
@@ -61,17 +61,17 @@ class Editor:
             self.mapa_jogo.carregar(nome_arquivo)
 
     # ------------------------------- Interfaçe Visual --------------------------------------------
-    def renderizar_tile_atual(self):
+    def renderizar_tile_atual(self, mpos, pos_tile):
         img_tile = self.assets[self.lista_tiles[self.grupo_tile]][self.variante_tile].copy()
         img_tile.set_alpha(128)
-        self.display.blit(img_tile, (5, 5))
+        self.display.blit(img_tile, POS_TILE_ATUAL)
         if not self.grid_ativo:
-            self.display.blit(img_tile, self.mpos)
+            self.display.blit(img_tile, mpos)
         else:
-            pos = (self.pos_tile[0] * self.tamanho_tile - self.scroll[0],
-                   self.pos_tile[1] * self.tamanho_tile - self.scroll[1])
+            pos = (pos_tile[0] * TILE_SIZE - self.scroll[0],
+                   pos_tile[1] * TILE_SIZE - self.scroll[1])
             self.display.blit(img_tile, pos)
-            pygame.draw.rect(self.display, (255, 255, 255), (pos[0], pos[1], self.tamanho_tile, self.tamanho_tile), 1)
+            pygame.draw.rect(self.display, BRANCO, (pos[0], pos[1], TILE_SIZE, TILE_SIZE), 1)
 
     def desenhar_sidebar(self):
         surf_sidebar = pygame.Surface((70, self.display.get_height()), pygame.SRCALPHA)
@@ -82,24 +82,24 @@ class Editor:
         self.display.blit(surf_sidebar, (self.display.get_width() - 70, 0))
 
     def grids_editor(self):
-        deslocamento_grid = (self.scroll[0] % self.tamanho_tile, self.scroll[1] % self.tamanho_tile)
+        deslocamento_grid = (self.scroll[0] % TILE_SIZE, self.scroll[1] % TILE_SIZE)
         surf_grid = pygame.Surface(self.display.get_size(), pygame.SRCALPHA)
 
         pygame.draw.line(surf_grid, (0, 255, 255),
-                         (-self.scroll[0], 0), (-self.scroll[0], self.display.get_height()), 3)
+                         (-self.scroll[0], 0), (-self.scroll[0], DISPLAY_A), 3)
         pygame.draw.line(surf_grid, (0, 255, 255),
-                         (0, -self.scroll[1]), (self.display.get_width(), -self.scroll[1]), 3)
+                         (0, -self.scroll[1]), (DISPLAY_L, -self.scroll[1]), 3)
 
         # Linhas Horizontais
-        for x in range(self.display.get_width() // self.tamanho_tile + 1):
-            pygame.draw.line(surf_grid, (100, 100, 100, 180), (x * self.tamanho_tile - deslocamento_grid[0], 0),
-                             (x * self.tamanho_tile - deslocamento_grid[0], self.display.get_height()))
+        for x in range(DISPLAY_L // TILE_SIZE + 1):
+            pygame.draw.line(surf_grid, CINZA, (x * TILE_SIZE - deslocamento_grid[0], 0),
+                             (x * TILE_SIZE - deslocamento_grid[0], DISPLAY_A))
         # Linhas Verticais
-        for y in range(self.display.get_height() // self.tamanho_tile + 1):
-            pygame.draw.line(surf_grid, (100, 100, 100), (0, y * self.tamanho_tile - deslocamento_grid[1]),
-                             (self.display.get_width(), y * self.tamanho_tile - deslocamento_grid[1]))
+        for y in range(DISPLAY_A // TILE_SIZE + 1):
+            pygame.draw.line(surf_grid, CINZA, (0, y * TILE_SIZE - deslocamento_grid[1]),
+                             (DISPLAY_L, y * TILE_SIZE - deslocamento_grid[1]))
 
-        surf_grid.set_alpha(100)
+        surf_grid.set_alpha(TRANSPARENCIA_GRID)
         self.display.blit(surf_grid, (0, 0))
 
     # ---------------------------------------------------------------------------------------------
@@ -118,64 +118,63 @@ class Editor:
                 self.grupo_tile = (self.grupo_tile + 1) % len(self.lista_tiles)
                 self.variante_tile = 0
 
-    def adicionar_tiles(self):
-        pos_offgrid = (self.mpos[0] + self.scroll[0], self.mpos[1] + self.scroll[1])
+    def adicionar_tiles(self, mpos, pos_tile):
+        pos_offgrid = (mpos[0] + self.scroll[0], mpos[1] + self.scroll[1])
         tipo_tile_atual = self.lista_tiles[self.grupo_tile]
         if self.clique and self.grid_ativo:
-            self.mapa_jogo.tilemap[str(self.pos_tile[0]) + ';' + str(self.pos_tile[1])] =\
-                {'tipo': tipo_tile_atual, 'variante': self.variante_tile, 'pos': self.pos_tile}
+            self.mapa_jogo.tilemap[str(pos_tile[0]) + ';' + str(pos_tile[1])] =\
+                {'tipo': tipo_tile_atual, 'variante': self.variante_tile, 'pos': pos_tile}
 
         if self.clique and not self.grid_ativo:
             self.clique = False
             self.mapa_jogo.offgrid_tiles.append(
                 {'tipo': tipo_tile_atual, 'variante': self.variante_tile, 'pos': pos_offgrid})
 
-    def remover_tiles(self):
+    def remover_tiles(self, mpos, pos_tile):
         if self.clique_direito:
-            loc_tile = str(self.pos_tile[0]) + ';' + str(self.pos_tile[1])
+            loc_tile = str(pos_tile[0]) + ';' + str(pos_tile[1])
             if loc_tile in self.mapa_jogo.tilemap:
                 del self.mapa_jogo.tilemap[loc_tile]
             for tile in self.mapa_jogo.offgrid_tiles.copy():
                 img_tile = self.assets[tile['tipo']][tile['variante']]
                 r_tile = pygame.Rect(tile['pos'][0] - self.scroll[0], tile['pos'][1] - self.scroll[1],
                                      img_tile.get_width(), img_tile.get_height())
-                if r_tile.collidepoint(self.mpos):
+                if r_tile.collidepoint(mpos):
                     self.mapa_jogo.offgrid_tiles.remove(tile)
 
     def movimento_camera(self):
-        self.scroll[0] += (self.movimento[1] - self.movimento[0]) * 5
-        self.scroll[1] += (self.movimento[3] - self.movimento[2]) * 5
+        self.scroll[0] += (self.movimento[1] - self.movimento[0]) * VEL_CAMERA
+        self.scroll[1] += (self.movimento[3] - self.movimento[2]) * VEL_CAMERA
         self.camera = (int(self.scroll[0]), int(self.scroll[1]))
 
     def mouse_tile_pos(self):
-        self.mpos = pygame.mouse.get_pos()
-        self.mpos = (self.mpos[0] / ESCALA_RENDER, self.mpos[1] / ESCALA_RENDER)
-        self.pos_tile = (int((self.mpos[0] + self.scroll[0]) // self.tamanho_tile),
-                         int((self.mpos[1] + self.scroll[1]) // self.tamanho_tile))
+        mpos = pygame.mouse.get_pos()
+        mpos = (mpos[0] / ESCALA_RENDER, mpos[1] / ESCALA_RENDER)
+        pos_tile = (int((mpos[0] + self.scroll[0]) // TILE_SIZE),
+                    int((mpos[1] + self.scroll[1]) // TILE_SIZE))
+        return mpos, pos_tile
 
     def atualizar(self):
         self.movimento_camera()
-        self.mouse_tile_pos()
-        self.adicionar_tiles()
-        self.remover_tiles()
+        self.m_pos, self.pos_tile = self.mouse_tile_pos()
+        self.adicionar_tiles(self.m_pos, self.pos_tile)
+        self.remover_tiles(self.m_pos, self.pos_tile)
 
     def renderizar(self):
-        self.display.fill((0, 0, 0))
+        self.display.fill(PRETO)
         self.mapa_jogo.renderizar(self.display, deslocamento=self.camera)
         self.grids_editor()
-       # self.desenhar_sidebar()
-        self.renderizar_tile_atual()
+        #self.desenhar_sidebar()
+        self.renderizar_tile_atual(self.m_pos, self.pos_tile)
 
         self.tela.blit(pygame.transform.scale(
         self.display, self.tela.get_size()), (0, 0))
-        pygame.display.update()
-        self.relogio.tick(60)
+
 
     def checar_eventos(self):
         for evento in pygame.event.get():
-            if evento.type == pygame.QUIT or (evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE):
-                pygame.quit()
-                sys.exit()
+            if evento.type == pygame.QUIT:
+                self.rodando = False
             elif evento.type == pygame.MOUSEBUTTONDOWN:
                 if evento.button == 1:
                     self.clique = True
@@ -188,7 +187,9 @@ class Editor:
                 elif evento.button == 3:
                     self.clique_direito = False
             if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_a:
+                if evento.key == pygame.K_ESCAPE:
+                    self.rodando = False
+                elif evento.key == pygame.K_a:
                     self.movimento[0] = True
                 elif evento.key == pygame.K_d:
                     self.movimento[1] = True
@@ -203,7 +204,7 @@ class Editor:
                 elif evento.key == pygame.K_t:
                     self.mapa_jogo.autotile()
                 elif evento.key == pygame.K_o:
-                    self.mapa_jogo.salvar('mapa.json')
+                    self.mapa_jogo.salvar(NOME_SAVE)
                 elif evento.key == pygame.K_p:
                     self.pesquisar_mapas()
             elif evento.type == pygame.KEYUP:
@@ -219,10 +220,14 @@ class Editor:
                     self.shift = False
 
     def rodar(self):
-        while True:
+        while self.rodando:
             self.checar_eventos()
             self.atualizar()
             self.renderizar()
+            pygame.display.update()
+            self.relogio.tick(FPS)
+        pygame.quit()
+        sys.exit()
 
 if __name__ == '__main__':
     Editor().rodar()
---
+++ scripts/editor_config.py
@@ -0,0 +1,30 @@
+
+# Parâmetros iniciais do editor
+EDITOR_RES = LARGURA, ALTURA = 1280, 720
+DISPLAY_L, DISPLAY_A = 640, 360
+LEGENDA = 'Editor de Levels'
+FPS = 60
+
+ESCALA_RENDER = 2.0
+
+TRANSPARENCIA_GRID = 100
+
+# Velocidade de movimento da câmera
+VEL_CAMERA = 5
+
+# Cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+CINZA = (100, 100, 100)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
+AMARELO = (255, 255, 0)
+
+# Parâmetros do tilemap
+TILE_SIZE = 16
+
+
+NOME_SAVE = 'mapa.json'
+
+POS_TILE_ATUAL = (10, 10)
