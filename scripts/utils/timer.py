27d977bbfc649fc714c31e6fe359dd213a86f71d
Tiago Albuquerque <tiagoalbuquerque090@gmail.com>
2026-01-10 18:05:05 -0300

    Adicionado uma classe de temporizador

---
+++ scripts/utils/timer.py
@@ -0,0 +1,31 @@
+from pygame.time import get_ticks
+
+
+class Timer:
+    def __init__(self, duracao, repetir=False, auto_start=False, funcao=None):
+        self.duracao = duracao
+        self.tempo_inicial = 0
+        self.ativo = False
+        self.repetir = repetir
+        self.funcao = funcao
+        if auto_start:
+            self.ativar()
+
+    def ativar(self):
+        self.ativo = True
+        self.tempo_inicial = get_ticks()
+
+    def desativar(self):
+        self.ativo = False
+        self.tempo_inicial = 0
+        if self.repetir:
+            self.ativar()
+
+    def atualizar(self):
+        if self.ativo:
+            tempo_atual = get_ticks()
+            if tempo_atual - self.tempo_inicial >= self.duracao:
+                if self.funcao and self.tempo_inicial != 0:
+                    self.funcao()
+                self.desativar()
+
