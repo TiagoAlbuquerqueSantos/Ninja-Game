9cb6f9bdbfe68df61a3bf8968bd6e2bdb9867a02
Tiago Albuquerque <tiagoalbuquerque090@gmail.com>
2026-01-11 11:06:25 -0300

    Atualizando projeto

--- scripts/debug.py
+++ scripts/debug.py
@@ -28,7 +28,7 @@ class Debug:
                round(self.game.jogador.velocidade[1], 2))
 
         dados_texto = f"""
-        FPS: {self.game.relogio.get_fps():.2f} - Dt: {self.game.dt:.4f}
+        FPS: {self.game.relogio.get_fps():.2f}
         Pos: {pos}
         Vel: {vel}
         Nums de chances: {self.game.derrotado} - Tempo no Ar: {self.game.jogador.tempo_ar}
