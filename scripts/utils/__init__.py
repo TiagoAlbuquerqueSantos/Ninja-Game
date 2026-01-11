df90df085953a70cad8e0c727306a21735fcec55
Tiago Albuquerque <tiagoalbuquerque090@gmail.com>
2026-01-11 15:39:57 -0300

    Melhorando o sistema de projeteis para os inimigos

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,8 +1,10 @@
 
+from .timer import Timer
 from .animation import Animacao
 from .image_utils import carregar_imagem, carregar_imagens, aplicar_contornos
 
 __all__ = [
+    'Timer',
     'Animacao',
     'carregar_imagem',
     'carregar_imagens',
