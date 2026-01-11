4e712ffb78490e44f4bf19a66e2680338185a846
Tiago Albuquerque <tiagoalbuquerque090@gmail.com>
2026-01-11 10:52:51 -0300

    Revert "Criei o objeto de projeteis do inimigo"
    This reverts commit bd36205c

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,10 +1,8 @@
 
-from .timer import Timer
 from .animation import Animacao
 from .image_utils import carregar_imagem, carregar_imagens, aplicar_contornos
 
 __all__ = [
-    'Timer',
     'Animacao',
     'carregar_imagem',
     'carregar_imagens',
